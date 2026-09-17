b41625f337bf8cf24668e3f81819f823f5520f4b
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-17 07:04:47 -0400

    build: keep visual policy out of precompiled header

--- src/dll/src/pch.h
+++ src/dll/src/pch.h
@@ -113,7 +113,6 @@ using namespace Nilesoft;
 #include "Parser\Parser.h"
 #include "Include\Hooker.h"
 #include "Include\Initializer.h"
-#include "Include\WindowsVisualState.h"
 // Global variable definition
 // explicit declaration
 #include "Include\FindPattern.h"
@@ -128,13 +127,13 @@ using namespace Nilesoft;
 //#define ISOLATION_AWARE_ENABLED 1
 /*
 #if defined _M_IX86
-  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144cb311a299b373c0c1fe3' language='*'\"")
 #elif defined _M_IA64
-  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144cb311a299b373c0c1fe3' language='*'\"")
 #elif defined _M_X64
-  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144cb311a299b373c0c1fe3' language='*'\"")
 #else
-  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144cb311a299b373c0c1fe3' language='*'\"")
 #endif
 */
 
